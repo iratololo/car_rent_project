aaaa3776320396c037f76d8366eef2cdeb4fa509
iratololo <89348244+iratololo@users.noreply.github.com>
2024-04-19 17:59:09 +0000

    Deploying to gh-pages from @ iratololo/car_rent_project@c111f3330aeeb09a06cc05b619b8570c256597ca 🚀

---
+++ assets/Catalog-2526f783.js
@@ -0,0 +1,1 @@
+import{u as t,a as r,r as e,g as c,j as o}from"./index-5412f61c.js";const n=s=>s.cars.cars.items,u=()=>{const s=t(n),a=r();return console.log("cars :>> ",s),e.useEffect(()=>{a(c())},[a]),o.jsx("div",{children:"Catalog"})};export{u as default};
