51a3098c28f3aa14d2d76ba940782cd5709d5603
iratololo <iratololo1991@gmail.com>
2024-04-20 23:46:28 +0300

    fix

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -2,22 +2,11 @@ import CarItem from "../CarItem/CarItem";
 
 import {List} from "./CarsList.styled"
 
-import { selectorAllCars } from "../../redux/cars/selectors";
-import { selectorCars } from "../../redux/cars/selectors";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
 
-const CarsList = () => {
-  const cars = useSelector(selectorCars);
-  const [carsList, setCarsList] = useState(cars);
-
-  useEffect(() => {
-    setCarsList((prev)=>[...prev, ...cars])
-  }, [cars]);
-  
+const CarsList = ({cars}) => {
   return (
       <List>
-      {carsList.length>0 && carsList.map((item) => {
+      {cars.length>0 && cars.map((item) => {
         return <CarItem key={item._id} data={item} />
       })}
       </List>
--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -9,36 +9,44 @@ import { selectorCarsPage } from "../../redux/cars/selectors";
 import { useEffect, useState } from "react";
 import { getCarsThunk } from "../../redux/cars/thunks";
 import { changePage } from '../../redux/cars/sliceCars';
+import { selectorCars } from "../../redux/cars/selectors";
+import { selectorAllCars } from "../../redux/cars/selectors";
 
 const Catalog = () => {
   const page = useSelector(selectorCarsPage);
+  const cars = useSelector(selectorCars);
+  const total = useSelector(selectorAllCars);
   const dispatch = useDispatch();
 
+  console.log('cars :>> ', cars);
+  console.log('total :>> ', total);
+
   const [currentPage, setCurrentPage] = useState(page);
-  console.log('currentPage :>> ', currentPage);
+  const [carsList, setCarsList] = useState(cars);
 
   const handleOnClick = () => {
     setCurrentPage((prev) =>  prev + 1);
-    // dispatch(changePage(currentPage))
   };
 
   useEffect(() => {
     dispatch(getCarsThunk());
-    // setCurrentPage(page);
-    // dispatch(changePage(currentPage))
   }, [dispatch]);
 
   useEffect(() => {
     dispatch(changePage(currentPage))
   }, [dispatch, currentPage]);
+
+  useEffect(() => {
+    setCarsList((prev)=>[...prev, ...cars])
+  }, [cars]);
   
   return (
     <Container>
       <CatalogWrapper>
         <SideBar>sidebar</SideBar>
         <ListWrap>
-          <CarsList />
-         <LoadMore onClick={handleOnClick} type="button">Load more</LoadMore>
+          <CarsList cars={carsList} />
+          {carsList.length<total.length && <LoadMore onClick={handleOnClick} type="button">Load more</LoadMore>}
         </ListWrap>
       </CatalogWrapper>
     </Container>
